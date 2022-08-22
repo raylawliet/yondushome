8ea3c6ed8c4d26dc1881a429bc54527ec2aec974
raylawliet <raylawliet94@gmail.com>
2022-08-22 16:50:20 +0900

    0822실습_제이쿼리/자바 기초

---
+++ javascript/html.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#popup_btn').click(function(){
+        $('.opt_black').show();
+    });
+    $('#close_btn').click(function(){
+        $('.opt_black').hide();
+    });
+});
