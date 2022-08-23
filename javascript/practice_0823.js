6b1c85107d6e9da20c800a40ced285aa49a5e265
raylawliet <raylawliet94@gmail.com>
2022-08-23 17:30:33 +0900

    0823실습_css/addClass

---
+++ javascript/practice_0823.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#look_btn').click(function(){
+        $('.bg_black').show();
+    });
+    $('#close_btn').click(function(){
+        $('.bg_black').hide();
+    });
+});
---
+++ javascript/practice_0823_2.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#look_btn').click(function(){
+        $('#aaa').html('부가적인 설명입니다.');
+    });
+});
